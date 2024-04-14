a8c4643b329f9a6b71839a51a38c4d241768ad00
Catalin Gavan <catalingavan@gmail.com>
2024-04-14 09:10:46 +0200

    work in progress

---
+++ src/Serilog.Sinks.LogBee.AspNetCore/HttpContextRequestInfoProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Sinks.LogBee.RequestInfo;
+
+namespace Serilog.Sinks.LogBee.AspNetCore
+{
+    internal class HttpContextRequestInfoProvider : IRequestInfoProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public HttpContextRequestInfoProvider(
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public DateTime StartedAt => DateTime.UtcNow;
+        public Uri AbsoluteUri => new Uri(new Uri("http://web-app.com"), _httpContextAccessor.HttpContext.Request.Path.ToString());
+        public string HttpMethod => _httpContextAccessor.HttpContext.Request.Method;
+        public RequestProperties RequestProperties => new();
+        public ResponseProperties ResponseProperties => new();
+    }
+}
